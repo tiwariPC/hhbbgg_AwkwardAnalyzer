229b4c5b8d3e6c59e3fc447a7d178424f996af34
Shivam Raj <sraj@cmslpc318.fnal.gov>
2024-08-08 08:48:25 -0500

    updated normalisation

--- normalisation.py
+++ normalisation.py
@@ -1,24 +1,13 @@
 def getXsec(samplename):
-    samplename = str(samplename).split("/")[-1].replace(".root", "")
+    #samplename = str(samplename).split("/")[-1].replace(".root", "")
+    samplename = str(samplename).split("/")[-1]
     # Branching ratio
     BR_HToGG = 2.270e-03
     BR_HTobb = 5.824e-01
     BR_HTogg = 2.270e-03  # https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNYellowReportPageBR
     
-    # List of specific sample names with cross-section 1
-    nmssm_samples = [
-        "NMSSM_X300_Y100", "NMSSM_X400_Y80", "NMSSM_X550_Y100", "NMSSM_X600_Y80", "NMSSM_X700_Y100",
-        "NMSSM_X300_Y60", "NMSSM_X400_Y90", "NMSSM_X550_Y60", "NMSSM_X600_Y90", "NMSSM_X700_Y60",
-        "NMSSM_X300_Y70", "NMSSM_X400_Y95", "NMSSM_X550_Y70", "NMSSM_X600_Y95", "NMSSM_X700_Y70",
-        "NMSSM_X300_Y80", "NMSSM_X500_Y100", "NMSSM_X550_Y80", "NMSSM_X650_Y100", "NMSSM_X700_Y80",
-        "NMSSM_X300_Y90", "NMSSM_X500_Y60", "NMSSM_X550_Y90", "NMSSM_X650_Y60", "NMSSM_X700_Y90",
-        "NMSSM_X300_Y95", "NMSSM_X500_Y70", "NMSSM_X550_Y95", "NMSSM_X650_Y70", "NMSSM_X700_Y95",
-        "NMSSM_X400_Y100", "NMSSM_X500_Y80", "NMSSM_X600_Y100", "NMSSM_X650_Y80",
-        "NMSSM_X400_Y60", "NMSSM_X500_Y90", "NMSSM_X600_Y60", "NMSSM_X650_Y90",
-        "NMSSM_X400_Y70", "NMSSM_X500_Y95", "NMSSM_X600_Y70", "NMSSM_X650_Y95"
-    ]
-
-    if samplename in nmssm_samples:
+    #if samplename in nmssm_samples:
+    if "NMSSM_X" in samplename:
         xsec = 1.0
     elif "GGJets" in samplename:
         xsec = 88.75
