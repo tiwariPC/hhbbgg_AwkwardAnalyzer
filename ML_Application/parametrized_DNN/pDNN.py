6baad289f0b481cf1ef62e425184bd507c956682
raj2022 <shivam.raj@niser.ac.in>
2025-07-24 09:23:41 +0200

    updated pDNN

--- ML_Application/parametrized_DNN/pDNN.py
+++ ML_Application/parametrized_DNN/pDNN.py
@@ -1,6 +1,6 @@
 # parametrized DNN for the B2G 
 
-
+import os
 # importing all modules
 import uproot
 import numpy as np
@@ -21,4 +21,53 @@ from torch.optim import Adam
 mass_points = [ 300, 400]
 
 # correspondng Y Values
+y_values = [60, 70, 80, 90, 95, 100, 125, 150, 200]
 
+
+# Load signal data from Parquet files
+signal_data = []
+for mass in mass_points:
+    for y in y_values:
+        file_path = f"../../../output_parquet/final_production_Syst/merged/NMSSM_X{mass}_Y{y}/nominal/NOTAG_merged.parquet"
+        
+        if os.path.exists(file_path):  # Check if file exists
+            try:
+                df = pd.read_parquet(file_path)  # Load the Parquet file
+                df["mass"] = mass  
+                df["y_value"] = y  # Store Y value if needed
+                df["label"] = 1  # Assuming signal label
+                signal_data.append(df)
+            except Exception as e:
+                print(f"Warning: Could not read {file_path}. Error: {e}")
+        else:
+            print(f"Warning: File {file_path} does not exist.")
+
+# Combine all signal data into a single DataFrame
+signal_df = pd.concat(signal_data, ignore_index=True) if signal_data else pd.DataFrame()
+
+print("###################################")
+print("signal shape", signal_df )
+
+
+# Load background data from ROOT files
+background_files = [
+    ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GGJets/preselection"),
+    ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GJetPt20To40/preselection"),
+    ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GJetPt40/preselection"),
+#     ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GGJets/preselection"),
+#     ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GJetPt20To40/preselection"),
+#     ("../../outputfiles/hhbbgg_analyzer-v2-trees.root", "/GJetPt40/preselection"),
+]
+background_data = []
+for file_path, tree_name in background_files:
+    try:
+        with uproot.open(file_path) as file:
+            tree = file[tree_name]
+            df = tree.arrays(library="pd")
+#             df["mass"] = np.random.choice(mass_points, len(df))  # Random mass assignment
+            df["label"] = 0
+            background_data.append(df)
+    except Exception as e:
+        print(f"Warning: Could not read {file_path}. Error: {e}")
+
+df_background = pd.concat(background_data, ignore_index=True) if background_data else pd.DataFrame()
