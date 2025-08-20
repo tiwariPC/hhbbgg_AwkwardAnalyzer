2c5085991b3d4b575a31ecd3389435b3d3429039
raj2022 <shivam.raj@niser.ac.in>
2025-08-20 11:59:15 +0200

    updated PDNN Transfomed score

---
+++ ML_Application/parametrized_DNN/pDNN_score.py
@@ -0,0 +1,9 @@
+# PDNN trnsformed score on the saved outputs. 
+
+# 1. check the saved model from the traning
+# 2. On the saved model, check the samples.
+# 3.  On the transformed score, include it as variables in each sample
+# 4. process it as Data/MC comaprison
+
+
+
